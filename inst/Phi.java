d12d6381c73b0b0182dc033b4418a4eb9b0ace83
anonymous <anonymous@example.org>
2023-07-16 14:50:41 +0800

    phi ret add method getOpString.

--- inst/Phi.java
+++ inst/Phi.java
@@ -3,4 +3,8 @@ package moe.wjk.ir.inst;
 import moe.wjk.ir.Instruction;
 
 public class Phi extends Instruction {
+    @Override
+    public String getOpString() {
+        return "phi";
+    }
 }
--- inst/Ret.java
+++ inst/Ret.java
@@ -3,4 +3,8 @@ package moe.wjk.ir.inst;
 import moe.wjk.ir.Instruction;
 
 public class Ret extends Instruction {
+    @Override
+    public String getOpString() {
+        return "ret";
+    }
 }
